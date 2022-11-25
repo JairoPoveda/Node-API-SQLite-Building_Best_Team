5cf5f747f3a9d038927cb53621b6ac3de9a2ff72
unknown <jairopoveda2022@hotmail.com>
2022-11-25 08:14:40 +0800

    first commit

---
+++ src/api/player/delete.js
@@ -0,0 +1,34 @@
+// ---------------------------------------------------------------------------------------------
+// YOU CAN FREELY MODIFY THE CODE BELOW IN ORDER TO COMPLETE THE TASK
+// ---------------------------------------------------------------------------------------------
+
+import models from '../../db/model';
+
+export default async (req, res) => {
+
+  var playerID = (req.params.id).replace(':', '');  
+  
+  const player = await models.Player.findOne({ where: { id: playerID } });
+
+  // Checking if player exists in db  
+  if (player) {   
+    // inspecting token  
+    let token = req.headers.authorization;  
+    
+    /* 
+    
+    if(token !== 'xxxx') {    
+      return res.status(403).send({
+        message: 'No token provided! The selected player was protected with token.'
+      });
+    }
+    
+    */
+    await player.destroy();
+  } else {
+    return res.status(404).send({ message: `Invalid value for player id : ${playerID}` });
+  }
+
+  res.sendStatus(204);
+  
+}
